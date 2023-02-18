b1d05d4e4ac27f71abb0b8fe661f327638547adb
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-02-18 09:25:28 -0300

    Instalando o componente

---
+++ migrations-db.php
@@ -0,0 +1,7 @@
+<?php
+// O retorno foi copiado do arquivo EntityManagerCreator.php.
+return [
+    'driver' => 'pdo_sqlite',
+    // DB vai ser guardado na raiz do projeto.
+    'path' => __DIR__ . '/db.sqlite', 
+];
