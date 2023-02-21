989c5526a8f35dc1fe0ba9ba1d60f142d52871c7
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-02-21 10:35:56 -0300

    Metadata cache

--- src/Helper/EntityManagerCreator.php
+++ src/Helper/EntityManagerCreator.php
@@ -5,6 +5,7 @@ namespace Alura\Doctrine\Helper;
 use Doctrine\DBAL\Logging\Middleware;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
@@ -24,6 +25,12 @@ class EntityManagerCreator
         $config->setMiddlewares([
             $logMiddleware,
         ]);
+
+        $config->setMetadataCache(new PhpFilesAdapter(
+            namespace: 'metadata_cache', // Pasta que contém o cache.
+            // defaultLifetime: 0, // Não expira.
+            directory: __DIR__ . '/../../var/cache' // Caminho para o namespace.
+        ));
 
         $conn = [
             'driver' => 'pdo_sqlite',
